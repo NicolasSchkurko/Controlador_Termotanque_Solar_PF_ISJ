5ae2f2e6dd3db41ef9cd9cd2e9a6eb934bd3e466
chuco <chucoidleu@gmail.com>
2022-04-27 19:44:04 -0300

    proteus proyecto fina

--- other stuff/ino/Ejercicio 1 Embebidos Correccion/src/main.cpp
+++ other stuff/ino/Ejercicio 1 Embebidos Correccion/src/main.cpp
@@ -1,14 +1,26 @@
 #include <Arduino.h>
 
+#define TIEMPO_VELOCIDAD_1 500 //tiempo en ms
+#define TIEMPO_VELOCIDAD_2 50 //tiempo en ms
+
+typedef enum{velocidaduno,velocidaddos}ESTADOMEF;
+
+void Parpadeo();
+void Calc_tiempo();
+
+ESTADOMEF info_estado = velocidaduno;
 volatile unsigned int cuenta = 0;
+volatile unsigned int tiempo=0;
 bool ESTADO = false;
+char tiempo1=0;
+char tiempo2=0;
+
 const int pulsador1 = 2;
 const int pulsador2 = 3;
 const int led = 5;
-int info_estado = 0;
-volatile unsigned int tiempo=0;
 
 void setup() {
+Calc_tiempo();
 pinMode(led,OUTPUT);
 pinMode(pulsador1,INPUT);
 pinMode(pulsador2,INPUT);
@@ -16,27 +28,46 @@ SREG = (SREG & 0b01111111); //Desabilitar interrupciones
 TIMSK2 = TIMSK2|0b00000001; //Habilita la interrupcion por desbordamiento
 TCCR2B = 0b00000111; //Configura preescala para que FT2 sea de 7812.5Hz
 SREG = (SREG & 0b01111111) | 0b10000000; //Habilitar interrupciones //Desabilitar interrupciones
-Serial.begin(9600);
 }
+
 void loop() {
+Parpadeo(); 
   if(digitalRead(pulsador1) == HIGH)
 {
-  info_estado = 1;
-  tiempo=15;
+  info_estado = velocidaduno;
 }
 
 if(digitalRead(pulsador2) == HIGH)
 {
-  info_estado = 2;
-  tiempo=8;
+  info_estado = velocidaddos;
+}
 }
 
-}
-ISR(TIMER2_OVF_vect){
-cuenta++;
-if(cuenta>tiempo){
+void Parpadeo(){
+switch (info_estado) //cambia velocidades de parpadeo 
+    {
+    case velocidaddos:
+        tiempo=tiempo1;
+      break;
+  
+    case velocidaduno:
+       tiempo=tiempo2;
+     break;
+    }; 
+
+if(cuenta>tiempo) //enciende/apaga LED segun tiempo establecido
+{
   digitalWrite(led, ESTADO);
   ESTADO = !ESTADO;
   cuenta = 0;
 }
+}
+
+void Calc_tiempo(){
+tiempo1=TIEMPO_VELOCIDAD_1/37,7;
+tiempo2=TIEMPO_VELOCIDAD_2/37,7;
+}
+
+ISR(TIMER2_OVF_vect){
+cuenta++;
 }
