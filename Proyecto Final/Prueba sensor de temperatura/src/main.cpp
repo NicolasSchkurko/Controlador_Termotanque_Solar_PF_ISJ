408b5622793e17b5987312b7c3b5ac0d3992e1dd
jeremias.bartolsic <jeremias.bartolsic@gmail.com>
2022-05-15 17:20:48 -0300

    Sensor temp

---
+++ Proyecto Final/Prueba sensor de temperatura/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <OneWire.h>
+#include <LiquidCrystal.h>
+#include <DallasTemperature.h>
+
+LiquidCrystal lcd(12,11,10,9,8,7);
+
+const int sensor = 4;
+
+OneWire onewire(sensor);
+
+DallasTemperature sensor_temp(&onewire);
+
+void setup() 
+{
+  lcd.begin(16,2);
+  lcd.print("Prueba temp");
+  sensor_temp.begin();
+}
+
+void loop() 
+{
+  lcd.clear();
+  sensor_temp.requestTemperatures();
+  lcd.print(sensor_temp.requestTemperaturesByIndex(0));
+}
