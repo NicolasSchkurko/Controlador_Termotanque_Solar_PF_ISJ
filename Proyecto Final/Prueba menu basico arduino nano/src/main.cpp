c1e455bcb68381287d967ff4c9893dfbd1632342
jeremias.bartolsic <jeremias.bartolsic@gmail.com>
2022-05-18 23:55:15 -0300

    Cambios menu

--- Proyecto Final/Prueba menu basico arduino nano/src/main.cpp
+++ Proyecto Final/Prueba menu basico arduino nano/src/main.cpp
@@ -6,12 +6,14 @@ void imprimir_en_pantalla();
 //optimizar
 LiquidCrystal lcd(12,11,10,9,8,7);
 //Pines que usa el menu para manejarse
+const int pulsador6 = 0; //pulsador de retorno
+const int pulsador5 = 1;
 const int pulsador1 = 2;
 const int pulsador2 = 3;
 const int pulsador3 = 4;
 const int pulsador4 = 5;
 
-typedef enum{estado_inicial,Carga_x_sensor,Calienta_x_hora,Calienta_x_sensor,Calienta_manual,estado_5} estadoMEF;  
+typedef enum{estado_inicial,Carga_x_sensor,Calienta_x_hora,Calienta_x_sensor,Calienta_manual,llenado_manual} estadoMEF;  
 estadoMEF estadoActual;
 
 char variable_para_imprimir_en_el_lcd [20];
@@ -25,14 +27,26 @@ void setup()
   pinMode(pulsador2, INPUT_PULLUP);
   pinMode(pulsador3, INPUT_PULLUP);
   pinMode(pulsador4, INPUT_PULLUP);
+  pinMode(pulsador5, INPUT_PULLUP);
+  pinMode(pulsador6, INPUT_PULLUP);
 }
 
 void loop() 
 {
   if(digitalRead(pulsador1) == LOW) estadoActual = Carga_x_sensor;
+  if(digitalRead(pulsador1) == LOW) lcd.clear();
   if(digitalRead(pulsador2) == LOW) estadoActual = Calienta_x_hora;
+  if(digitalRead(pulsador2) == LOW) lcd.clear();
   if(digitalRead(pulsador3) == LOW) estadoActual = Calienta_x_sensor;
-  if(digitalRead(pulsador1) == LOW) estadoActual = Calienta_manual;
+  if(digitalRead(pulsador3) == LOW) lcd.clear();
+  if(digitalRead(pulsador4) == LOW) estadoActual = Calienta_manual;
+  if(digitalRead(pulsador4) == LOW) lcd.clear();
+  if(digitalRead(pulsador5) == LOW) estadoActual = llenado_manual;
+  if(digitalRead(pulsador5) == LOW) lcd.clear();
+  
+  if(digitalRead(pulsador6) == LOW) estadoActual = estado_inicial;
+  if(digitalRead(pulsador6) == LOW) lcd.clear();
+  
   actualizar_MEF();
 }
 
@@ -42,6 +56,8 @@ void actualizar_MEF()
   switch (estadoActual)
   {
     case estado_inicial:
+      lcd.print("1       2      3"); lcd.setCursor(0,2); lcd.print("4              5");
+    case llenado_manual:
       strcpy(variable_para_imprimir_en_el_lcd, "Llenado manual");
       lcd.print(variable_para_imprimir_en_el_lcd);
       //llenado_manual();
@@ -55,17 +71,20 @@ void actualizar_MEF()
       strcpy(variable_para_imprimir_en_el_lcd, "Calienta x hora");
       lcd.print(variable_para_imprimir_en_el_lcd);
       // calentamiento_por_hora();
+      break;
     case Calienta_x_sensor:
       strcpy(variable_para_imprimir_en_el_lcd, "Calienta x sensor");
       lcd.print(variable_para_imprimir_en_el_lcd);
       // calienta_por_sensor();
+      break;
     case Calienta_manual:
       strcpy(variable_para_imprimir_en_el_lcd, "Calienta manual");
       lcd.print(variable_para_imprimir_en_el_lcd);
       // calefaccion_manual();
+      break;
   }
 }
 
-void imprimir_en_pantalla(){
+/*void imprimir_en_pantalla(){
   lcd.print(variable_para_imprimir_en_el_lcd);
-}
+}*/
