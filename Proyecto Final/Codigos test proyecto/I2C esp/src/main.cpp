c1aae7d0b4735cca62f17f5785e7bf04fa075e60
chucoidle <chucoidle@gmail.com>
2022-06-27 18:06:52 -0300

    buenardo

--- Proyecto Final/Codigos test proyecto/I2C esp/src/main.cpp
+++ Proyecto Final/Codigos test proyecto/I2C esp/src/main.cpp
@@ -4,19 +4,22 @@ char conversionSave(char,const char*);
 struct guardado{ char pos; char hour; char level; char temp;};
 
 void setup() {
+  Serial.begin(9600);
 conversionSave(1,"12:45");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+conversionSave(1,"12:45");
 }
 
 char conversionSave(char func,const char* str){
 String data=str;
+char pablo=023;
 if (func==1){
   int i=0;
   for(int i=0;i<sizeof(data);i++){
-    Serial.println(str[i]);
+    Serial.println(str[i]);//imprime cada uno de los caracteres
  }
 }
+return pablo;
 }
