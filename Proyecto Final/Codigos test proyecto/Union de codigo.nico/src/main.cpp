62486465cd0eaeb6a46c9fe99eb284f6c6f181f4
chuco <chucoidleu@gmail.com>
2022-07-02 17:18:41 -0300

    union codigo.nico menues mas sencillos

--- Proyecto Final/Codigos test proyecto/Union de codigo.nico/src/main.cpp
+++ Proyecto Final/Codigos test proyecto/Union de codigo.nico/src/main.cpp
@@ -46,20 +46,21 @@ const uint8_t nivel_del_tanque = A0;
 const uint8_t electrovalvula = 10;
 const uint8_t resistencia = 11;
 uint8_t  nivel = 0;
-uint8_t  mili_segundos = 0;
+uint16_t  mili_segundos = 0;
 uint8_t sumador_nivel = 25;
-uint8_t  milis_para_nivel = 0;
-uint8_t tiempo_para_nivel = 3000;
+uint16_t  milis_para_nivel = 0;
+uint16_t tiempo_para_nivel = 3000;
 bool confirmar_nivel = false;
 //
 
 //cosas del menu princial
+uint8_t menuposY(uint8_t , uint8_t);
 void menu_basico();
 void standby();
 void imprimir_en_pantalla();
 void carga_por_nivel();
 void limpiar_pantalla_y_escribir_nivel();
-uint16_t tiempo_de_standby = 10000;
+uint16_t tiempo_de_standby = 0;
 //
 
 //cosas del mennu avanzado
@@ -74,10 +75,11 @@ void imprimir_hora ();
 
 //Cosas necesarias para el menu
 LiquidCrystal_I2C lcd(0x27,20,4);//LiquidCrystal_I2C lcd(0x20,20,4);
-typedef enum{estado_standby,estado_inicial,calefaccion_manual,calefaccion_auto_senstemp,carga_auto_hora,carga_agua_por_nivel,llenado_agua_manual} estadoMEF;  
-estadoMEF Menu_principal = estado_inicial;
-typedef enum{menu_inicio,set_wifi,activar_bomba,cambio_unidad,set_hora,momento_standby} estadoMEF2;
-estadoMEF2 Menu_secundario = menu_inicio;
+typedef enum{estado_standby,estado_inicial,menu1,menu2} estadoMEF;  
+estadoMEF Estadoequipo = estado_inicial;
+
+const uint8_t maxY_menu1=7;
+const uint8_t maxY_menu2=5;
 const uint8_t pulsador1 = 2;
 const uint8_t pulsador2 = 3;
 const uint8_t pulsador3 = 4;
@@ -85,6 +87,22 @@ const uint8_t pulsador4 = 5;
 const uint8_t pulsador5 = 6;
 const uint8_t pulsador6 = 7; //pulsador de retorno
 const uint8_t pulsador7 = 8;
+String Menuprincipal[maxY_menu1] = {
+  "carga manual",
+  "calentado manual",
+  "Cal. y carg. segun hora",
+  "calentado segun temp,",
+  "carga segun lleno,"
+  "menu avanzado",
+  "volver",
+};
+String menuavanzado[maxY_menu2] = {
+    "Setear hora",
+    "c° o F°",
+    "Activar la bomba",
+    "conexion wifi",
+    "volver",
+};
 bool flag_menu_avanzado = false;
 bool borrar_display = false;
 
@@ -133,229 +151,121 @@ void setup()
 
 void loop() 
 {
-  //now = RTC.now();
+  tomar_temperatura();
+  control_de_temp_auto();
+  sensar_nivel_actual();
   if (milis_para_nivel == tiempo_para_nivel)//sujeto a cambios
   {
     control_de_temp_auto();
     milis_para_nivel= 0;
   }
-
   if (milis_para_temperatura == tiempo_para_temperatura)
   {
     control_de_temp_auto();
     milis_para_temperatura = 0;
   }
-  // nico: QUE CHOTA ES ESTO AYUDAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-  if(flag_menu_avanzado == true)
+  switch (Estadoequipo)
   {
-    menu_avanzado();
-    Menu_principal = estado_inicial;
+    case estado_standby:
+      standby();
+      break;
+    case estado_inicial:
+      standby();
+      break;
+    case menu1:
+      menu_basico();
+      break;
+    case menu2:
+      menu_avanzado();
+      break;
   }
-
-  if(flag_menu_avanzado == false)
-  {
-    menu_basico();
-    Menu_secundario = menu_inicio;
-  } 
-
-  if(digitalRead(pulsador7) == LOW)
-  {
-    while(digitalRead(pulsador7) == LOW){}
-    lcd.clear();
-    mili_segundos = 0;
-    flag_menu_avanzado = !flag_menu_avanzado;
-  } 
-
-  if(digitalRead(pulsador6) == LOW) 
-  {
-    while (digitalRead(pulsador6) == LOW){}
-    mili_segundos = 0;
-    Menu_principal = estado_inicial;
-    Menu_secundario = menu_inicio;
-    lcd.clear();
-  }
-  if(mili_segundos >= tiempo_de_standby && digitalRead(pulsador1) == HIGH && digitalRead(pulsador2) == HIGH && digitalRead(pulsador3) == HIGH && digitalRead(pulsador4) == HIGH && digitalRead(pulsador5) == HIGH && digitalRead(pulsador6) == HIGH)
-  {
-    Menu_principal = estado_standby;
-    Menu_secundario = momento_standby; 
-    mili_segundos = 0;
-  }
+ 
 }
 
 void menu_basico()
 {
-  switch (Menu_principal)
+  uint8_t Ypos=0;
+  uint8_t posicionmenu=0;
+  if (digitalRead(pulsador1) == LOW) Ypos++;
+  if (digitalRead(pulsador2) == LOW) Ypos--;
+  if (digitalRead(pulsador3) == LOW) posicionmenu=Ypos+1;
+  switch (posicionmenu)
   {
-    case estado_standby:
-      standby();
-      if(digitalRead(pulsador1) == LOW) Menu_principal = estado_inicial;
-      if(digitalRead(pulsador2) == LOW) Menu_principal = estado_inicial;
-      if(digitalRead(pulsador3) == LOW) Menu_principal = estado_inicial;
-      if(digitalRead(pulsador4) == LOW) Menu_principal = estado_inicial;
-      if(digitalRead(pulsador5) == LOW) Menu_principal = estado_inicial;
+    case 0:
+      lcd.setCursor(1,0); 
+      lcd.print(Menuprincipal[Ypos]); 
+      lcd.setCursor(1,1); 
+      lcd.print(Menuprincipal[menuposY(Ypos+1,maxY_menu1)]); 
+      lcd.setCursor(1,2); 
+      lcd.print(Menuprincipal[menuposY(Ypos+2,maxY_menu1)]); 
+      lcd.setCursor(1,3); 
+      lcd.print(Menuprincipal[menuposY(Ypos+2,maxY_menu1)]);
       break;
-    case estado_inicial:
-      lcd.setCursor(0,0); lcd.print("Calef manual"); lcd.setCursor(0,1); lcd.print("Calef auto"); lcd.setCursor(0,2); lcd.print("Carga hora"); lcd.setCursor(0,3); lcd.print("Carga nivel"); //lcd.setCursor(16,4); lcd.print(Sensor_temp.getTempCByIndex(0));
-      if(digitalRead(pulsador1) == LOW )
-      { 
-        while (digitalRead(pulsador1) == LOW){}
-        mili_segundos = 0;
-        Menu_principal = calefaccion_manual;
-        lcd.clear();
-      }
-      if(digitalRead(pulsador2) == LOW) 
-      {
-        while (digitalRead(pulsador2) == LOW){}
-        mili_segundos = 0;
-        Menu_principal = calefaccion_auto_senstemp;
-        lcd.clear();
-      }
-      if(digitalRead(pulsador3) == LOW) 
-      {
-        while (digitalRead(pulsador3) == LOW){}
-        mili_segundos = 0;
-        Menu_principal = carga_auto_hora;
-        lcd.clear();
-      }
-      if(digitalRead(pulsador4) == LOW) 
-      {
-        while (digitalRead(pulsador4) == LOW){}
-        mili_segundos = 0;
-        Menu_principal = carga_agua_por_nivel;
-        lcd.clear();
-      }
-      if(digitalRead(pulsador5) == LOW) 
-      {
-        while (digitalRead(pulsador5) == LOW){}
-        mili_segundos = 0;
-        Menu_principal = llenado_agua_manual;
-        lcd.clear();
-      }
+    case 1:
+      //menu_de_llenado_manual();
       break;
-    case calefaccion_manual:
-      menu_de_calefaccion_manual();
+    case 2:
+     menu_de_calefaccion_manual();
       break;
-    case calefaccion_auto_senstemp:
-      lcd.setCursor(3,2); lcd.print("        "); lcd.setCursor(3,3);lcd.print("     "); lcd.setCursor(16,1); lcd.print("     ");
-      lcd.setCursor(0,0); lcd.print("Encender calefaccion");    lcd.setCursor(5,1); lcd.print("automatica"); lcd.setCursor(0,2); lcd.print("-Si"); lcd.setCursor(0,3); lcd.print("-No");
-      if(digitalRead(pulsador1) == LOW)//con las variables y el while le doy un delay para que se vea la confirmacion
-      {
-        while (digitalRead(pulsador1) == LOW){}//con las variables y el while le doy un delay para que se vea la confirmacion
-        int espera = 900;
-        unsigned long tiempo_ahora = 0;
-        tiempo_ahora = millis();
-        lcd.clear(); lcd.setCursor(8,1); lcd.print("DONE");
-        while (millis()< tiempo_ahora + espera){}
-        mili_segundos=0;
-        lcd.clear();
-        encendido_de_temp_auto = true;
-        Menu_principal = estado_inicial;
-      }
-      if(digitalRead(pulsador2) == LOW)//con las variables y el while le doy un delay para que se vea la confirmacion
-      {
-        while (digitalRead(pulsador2) == LOW){}
-        int espera = 900;
-        unsigned long tiempo_ahora = 0;
-        tiempo_ahora = millis();
-        lcd.clear(); lcd.setCursor(8,1); lcd.print("DONE");
-        while (millis()< tiempo_ahora + espera){}
-        mili_segundos=0;
-        lcd.clear();
-        encendido_de_temp_auto = false;
-        Menu_principal = estado_inicial;
-      }
+    case 3:
+      // cal y carg segun hora
       break;
-    case carga_auto_hora:
-      lcd.setCursor(0,0); lcd.print("Carga auto hora");
-      //Todavia no
+    case 4:
+      //calef segun tempmin
       break;
-    case carga_agua_por_nivel:
+    case 5:
       carga_por_nivel();
       break;
-    case llenado_agua_manual:
-      lcd.setCursor(0,0); lcd.print("Llenado manual");
-      // calefaccion_manual();
+    case 6:
+      Estadoequipo=menu2;
+      break;
+    case 7:
+      Estadoequipo=estado_inicial;
       break;
   }
 }
 
 void menu_avanzado()
 {
-  switch (Menu_secundario)
+  uint8_t Ypos=0;
+  uint8_t posicionmenu=0;
+  if (digitalRead(pulsador1) == LOW) Ypos++;
+  if (digitalRead(pulsador2) == LOW) Ypos--;
+  if (digitalRead(pulsador3) == LOW) posicionmenu=Ypos+1;
+  switch (posicionmenu)
   {
-    case momento_standby:
-      standby();
-      if(digitalRead(pulsador1) == LOW) Menu_secundario = menu_inicio;
-      if(digitalRead(pulsador2) == LOW) Menu_secundario = menu_inicio;
-      if(digitalRead(pulsador3) == LOW) Menu_secundario = menu_inicio;
-      if(digitalRead(pulsador4) == LOW) Menu_secundario = menu_inicio;
-      if(digitalRead(pulsador5) == LOW) Menu_secundario = menu_inicio;
-    break;  
-    
-    case menu_inicio:
-      lcd.setCursor(0,0); lcd.print("Setear wifi"); lcd.setCursor(0,1); lcd.print("Activar bomba"); lcd.setCursor(0,2); lcd.print("Cambio unidad"); lcd.setCursor(0,3); lcd.print("Setear hora");
-      if(digitalRead(pulsador1) == LOW )
-      { 
-        while (digitalRead(pulsador1) == LOW){}
-        mili_segundos = 0;
-        Menu_secundario = set_wifi;
-        lcd.clear();
-      }
-      if(digitalRead(pulsador2) == LOW) 
-      {
-        while (digitalRead(pulsador2) == LOW){}
-        mili_segundos = 0;
-        Menu_secundario = activar_bomba;
-        lcd.clear();
-      }
-      if(digitalRead(pulsador3) == LOW) 
-      {
-        while (digitalRead(pulsador3) == LOW){}
-        mili_segundos = 0;
-        Menu_secundario = cambio_unidad;
-        lcd.clear();
-      }
-      if(digitalRead(pulsador4) == LOW) 
-      {
-        while (digitalRead(pulsador4) == LOW){}
-        mili_segundos = 0;
-        Menu_secundario = set_hora;
-        lcd.clear();
-      }
-    break;
+    case 0:
+      lcd.setCursor(1,0); 
+      lcd.print(Menuprincipal[Ypos]); 
+      lcd.setCursor(1,1); 
+      lcd.print(Menuprincipal[menuposY(Ypos+1,maxY_menu2)]); 
+      lcd.setCursor(1,2); 
+      lcd.print(Menuprincipal[menuposY(Ypos+2,maxY_menu2)]); 
+      lcd.setCursor(1,3); 
+      lcd.print(Menuprincipal[menuposY(Ypos+2,maxY_menu2)]);
+      break;
+    case 1:
+      //setear hora
+      break;
+    case 2: 
+      // formato grados
+      break;
+    case 3:
+      // Activar bomba
+      break;
+    case 4:
+      // Conexion Wifi
+      break;
+    case 5:
+      Estadoequipo= menu1;
+      break;
 
-    case set_wifi:
-      lcd.setCursor(0,0); lcd.print("Seteo WIFI");
-    //setea wifi, falta programar
-    break;
-
-    case activar_bomba:
-      lcd.setCursor(0,0); lcd.print("Activar bomba");
-    //activa bomba de agua por relay, falta programar
-    break;
-
-    case cambio_unidad:
-      lcd.setCursor(0,0); lcd.print("Cambio de unidad");
-    //cambia unidad de temperatura entre Celsius y Fahrenheit
-    break;
-
-    case set_hora:
-      lcd.setCursor(0,0); lcd.print("Seteo de hora");
-    //Seteo hora del RTC mediante pulsadores
-    break;
   }
 }
 
-
 void standby()
 {
-  lcd.setCursor(0,0); lcd.print("                    "); 
-  lcd.setCursor(0,1); lcd.print(" "); lcd.setCursor(18,1); lcd.print("  ");
-  lcd.setCursor(0,2); lcd.print(" "); lcd.setCursor(12,2); lcd.print("      "); 
-  lcd.setCursor(0,3); lcd.print("        "); lcd.setCursor(13,3); lcd.print("        ");
-  lcd.setCursor(1,2); 
   lcd.print("Nivel: ");
-  sensar_nivel_actual();
   switch (nivel_actual)
   {
     case tanque_vacio:
@@ -375,9 +285,30 @@ void standby()
       break;
   }
   //imprimir_hora();
-  lcd.setCursor(1,1);
-  tomar_temperatura();
-  control_de_temp_auto();
+  if(digitalRead(pulsador1)==LOW || digitalRead(pulsador2)==LOW || digitalRead(pulsador3)==LOW || digitalRead(pulsador4)==LOW || digitalRead(pulsador5)==LOW || digitalRead(pulsador6)==LOW || digitalRead(pulsador7)==LOW ){
+    switch (Estadoequipo)
+    {
+    case estado_standby:
+      Estadoequipo == estado_inicial;
+      lcd.backlight();
+      tiempo_de_standby=0;
+      break;
+    case estado_inicial:
+      Estadoequipo ==menu1;
+      lcd.backlight();
+      break;
+    default:
+      Estadoequipo = estado_standby;
+      break;
+    }   
+    }
+  if(tiempo_de_standby>=5000 && Estadoequipo  == estado_inicial)
+  {
+    Estadoequipo = estado_standby;
+    lcd.noBacklight();
+    tiempo_de_standby = 0;
+  }
+
 }
 
 void tomar_temperatura ()
@@ -386,21 +317,7 @@ void tomar_temperatura ()
   Sensor_temp.requestTemperatures();
   lcd.print(Sensor_temp.getTempCByIndex(0));
 }
-/*
-void imprimir_hora (){//ver que pasa
-  lcd.setCursor(8,3); 
-  lcd.print(now.hour(), DEC);
-  lcd.print(':');
-  lcd.print(now.minute(), DEC);
-  lcd.print(" ");
-}
-*/
 
-ISR(TIMER2_OVF_vect){
-    mili_segundos++;
-    milis_para_nivel++;
-    if(encendido_de_temp_auto == true) milis_para_temperatura++;
-}
 
 void sensar_nivel_actual(){
     if (analogRead(nivel_del_tanque) < 100) nivel_actual = tanque_vacio;  
@@ -498,7 +415,7 @@ void limpiar_pantalla_y_escribir (){
     tiempo_ahora = millis();
     while (millis()< tiempo_ahora + espera){}
     lcd.clear();
-    Menu_principal = estado_inicial;
+    Estadoequipo = estado_inicial;
     control_de_temp_auto();
   }
 }
@@ -600,10 +517,20 @@ void nivel_auto (){
   if (nivel_actual == tanque_al_100)    digitalWrite(electrovalvula, LOW);
 }
 
+uint8_t menuposY (uint8_t actualpos, uint8_t maxpos){
+uint8_t diferencepos = 0;
+if (actualpos>maxpos){
+  diferencepos = actualpos-maxpos;
+  return diferencepos;
+}
+  if (actualpos<=maxpos){
+  return actualpos;
+}
+}
 
-
-
-
-
-
-
+ISR(TIMER2_OVF_vect){
+    mili_segundos++;
+    milis_para_nivel++;
+    tiempo_de_standby++;
+    if(encendido_de_temp_auto == true) milis_para_temperatura++;
+}
