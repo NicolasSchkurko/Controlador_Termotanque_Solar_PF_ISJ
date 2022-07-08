92ed4bc6300413bcbe40cc8bf1f7d5c852995660
chuco <chucoidleu@gmail.com>
2022-07-07 22:43:46 -0300

    mejoras guardado rtc

--- Proyecto Final/Codigos test proyecto/Union de codigo.nico/src/main.cpp
+++ Proyecto Final/Codigos test proyecto/Union de codigo.nico/src/main.cpp
@@ -51,8 +51,10 @@ uint8_t sumador_nivel = 25;
 uint16_t  milis_para_nivel = 0;
 uint16_t tiempo_para_nivel = 3000;
 bool confirmar_nivel = false;
-//
-
+//cosas guardado y rtc
+struct save_data{ uint8_t hour; uint8_t level; uint8_t temp;};
+save_data save[5]; 
+uint8_t ActualStruct=0;
 //cosas del menu princial
 uint8_t menuposY(uint8_t , uint8_t);
 void menu_basico();
@@ -65,7 +67,22 @@ uint8_t opcionmenu1=0;
 uint8_t opcionmenu2=0;
 uint8_t Flag=0;
 //
-
+// Comunicacion esp/arduino
+void Serial_Read_UNO();
+void Serial_Send_UNO(uint8_t);
+String Serial_Input;
+String Individualdata[4];
+String IndividualValue;
+uint16_t Send_time=0;
+uint8_t StringLength=0;
+uint8_t ActualIndividualDataPos=0;
+char Actualchar=0;
+char input=0;
+bool ConvertString=false;
+bool StringTaked=false;
+bool stateheating=false;
+bool ComunicationError=false;
+bool InitComunication;
 //cosas del mennu avanzado
 void menu_avanzado();
 //
@@ -155,12 +172,13 @@ void setup()
   pinMode(pulsador6, INPUT_PULLUP);
   pinMode(pulsador7, INPUT_PULLUP);
   //
-  WIFISSID=" ";
+  InitComunication=true;
 }
 
 void loop() 
 {
   tomar_temperatura();
+  if (Serial.available()>0)Serial_Read_UNO();
   /*control_de_temp_auto();
   sensar_nivel_actual();*/
   /*if (milis_para_nivel == tiempo_para_nivel)//sujeto a cambios
@@ -188,6 +206,7 @@ void loop()
   }
  
 }
+
 void standby()
 { 
   lcd.setCursor(0,0);
@@ -355,13 +374,202 @@ else{
 }
 }
 
+void Serial_Read_UNO(){
+  
+  Serial_Input=Serial.readString();// iguala el string del serial a un string imput
+  StringLength= Serial_Input.length();// saca el largo del string
+  Serial.println(Serial_Input);// Solo de verificacion (eliminar en el final)
+  input=Serial_Input.charAt(0); // toma el char de comando (el primer char usualmente una letra)
+
+  for (uint8_t CharPos = 2; CharPos <= StringLength; CharPos++) // comeinza desde la posicion 2 del char (tras el _) y toma todos los datos
+  {
+      if(Serial_Input.charAt(CharPos)==':')ActualIndividualDataPos++; //si hay : divide los datos
+      if(Serial_Input.charAt(CharPos)!=':' && Serial_Input.charAt(CharPos)!='_' && Serial_Input.charAt(CharPos)!='/')// si no es nungun caracter especial:
+        {
+          if(Serial_Input.charAt(CharPos-1)==':' || Serial_Input.charAt(CharPos-1)=='_')Individualdata[ActualIndividualDataPos]=Serial_Input.charAt(CharPos);//si es el primer digito lo iguala
+          else Individualdata[ActualIndividualDataPos]+=Serial_Input.charAt(CharPos);//si es el segundo en adelante lo suma
+        }
+      if(CharPos==StringLength)ConvertString=true;// activa el comando final (flag)
+  }
+
+  switch (input)//dependiendo del char de comando
+  {
+  case 'H':
+    if (ConvertString==true)
+      {
+        //temp_a_calentar=Individualdata[0].toInt();
+        if(Individualdata[1].toInt()>=1)Serial.println("on");//save ycalentado manual();
+        else Serial.println("off");//only save
+      }
+    break;
+
+  case 'C':
+    if (ConvertString==true)
+      {
+        //nivel_a_llenar=Individualdata[0].toInt();
+        if(Individualdata[1].toInt()>=1)Serial.println("on");//save ycalentado manual();
+        else Serial.println("off");//only save
+        ActualIndividualDataPos=0;
+        StringTaked=false;
+        ConvertString=false;
+        ComunicationError=false;
+      }
+    break;
+
+  case 'K':
+    if (ConvertString==true)
+      {
+        ActualStruct=Individualdata[3].toInt();
+        if (ActualStruct<=2 && ActualStruct>=0)
+          {
+            save[ActualStruct].hour=Individualdata[0].toInt();
+            save[ActualStruct].temp=Individualdata[1].toInt();
+            save[ActualStruct].level=Individualdata[2].toInt();
+            ActualIndividualDataPos=0;
+            StringTaked=false;
+            ConvertString=false;
+            ComunicationError=false;
+          }
+        else Serial.println("error");
+      }
+    break;
+
+  case 'J':
+    if (ConvertString==true)
+      {
+        save[3].hour=255;
+        save[4].temp=Individualdata[1].toInt();// tempmin
+        save[4].level=Individualdata[2].toInt();// tempmax
+        ActualIndividualDataPos=0;
+        StringTaked=false;
+        ConvertString=false;
+        ComunicationError=false;
+      }
+    break;
+      case 'V':
+    if (ConvertString==true)
+      {
+        save[4].hour=255;
+        save[4].temp=Individualdata[1].toInt();// lvlmin
+        save[4].level=Individualdata[2].toInt();// lvlmax
+        ActualIndividualDataPos=0;
+        StringTaked=false;
+        ConvertString=false;
+        ComunicationError=false;
+      }
+    break;
+    case 'E':
+    if (ConvertString==true)
+      {
+        if(Individualdata[0]=="ERROR")
+        {
+              Serial.println("?_RESET");
+              ComunicationError=true;
+        }
+        ActualIndividualDataPos=0;
+        StringTaked=false;
+        ConvertString=false;
+      }
+    break;
+    case 'O':
+    if (ConvertString==true)
+      {
+        if(Individualdata[0]=="OK")
+        {
+              ComunicationError=false;
+        }
+        ActualIndividualDataPos=0;
+        StringTaked=false;
+        ConvertString=false;
+      }
+    break;
+  default:
+    Serial.println("?_NOTHING TO READ");
+    break;
+  }
+}
+
+void Serial_Send_UNO(uint8_t WhatSend)
+  {
+    uint8_t MessagePoss;
+    if (InitComunication==true)MessagePoss=0;
+    switch (WhatSend){
+      case 1:
+        if (ComunicationError==false && InitComunication==true && MessagePoss<=5 &&Send_time>=1000)
+        {
+            switch (MessagePoss)
+            {
+              case 0:
+                Serial.println("S_""SSID"":""Pass");
+                 MessagePoss++;
+                 Send_time=0;
+                break;
+              case 1:
+                Serial.println("K_HORA:TEMP:LVL:0");
+                MessagePoss++;
+                Send_time=0;
+                break;
+              case 2:
+                Serial.println("K_HORA:TEMP:LVL:1");
+                MessagePoss++;
+                Send_time=0;
+                break;
+              case 3:
+                Serial.println("K_HORA:TEMP:LVL:2");
+                MessagePoss++;
+                Send_time=0;
+                break;
+              case 4:
+                Serial.println("J_255:TEMPMIN:TEMPMAX:3");
+                MessagePoss++;
+                Send_time=0;
+                break;
+              case 5:
+                Serial.println("V_255:TEMPMIN:TEMPMAX:3");
+                InitComunication=false;
+                MessagePoss=0;
+                Send_time=0;
+                break;
+            // delay de 1 seg
+            }
+
+            //Send_time =0;
+        }
+        break;
+      case 2:
+        if (ComunicationError==false && InitComunication==false && Send_time>=2000 )
+          {
+              Serial.println("U_TEMP:LVL:HORA:STATE");
+              Send_time=0;
+          }
+      case 3:
+        if (ComunicationError==false && InitComunication==false)
+          {
+            Serial.println("K_HORA:TEMP:LVL:STRUCTPOS");
+          }
+      case 4:
+        if (ComunicationError==false && InitComunication==false)
+          {
+            Serial.println("J_255:TEMPMIN:TEMPMAX:3");
+          }
+      case 5:
+        if (ComunicationError==false && InitComunication==false)
+          {
+            Serial.println("V_255:LVLMIN:LVLMAX:4");
+          }
+    }
+    
+  }
+
 ISR(TIMER2_OVF_vect){
   mili_segundos++;
   milis_para_nivel++;
   tiempo_de_standby++;
   milis_para_temperatura++;
+  Send_time++;
   //if(encendido_de_temp_auto == true) 
 }
+
 /*======================PARA ADAPTAR=========================*/
 
 void tomar_temperatura () //Sexo y adaptarlo para no usar delay
