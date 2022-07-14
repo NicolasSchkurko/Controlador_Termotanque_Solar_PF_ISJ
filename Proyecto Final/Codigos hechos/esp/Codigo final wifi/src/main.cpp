2959fe30eec7b1f6fa443fc108079b3b4aab6f6e
chucoidle <chucoidle@gmail.com>
2022-07-14 18:53:47 -0300

    update wifi

--- Proyecto Final/Codigos hechos/esp/Codigo final wifi/src/main.cpp
+++ Proyecto Final/Codigos hechos/esp/Codigo final wifi/src/main.cpp
@@ -5,15 +5,19 @@
 #include <FS.h>
 #include <LittleFS.h>
 
-void Serial_Send_UNO(uint8_t);
+void Serial_Send_NODEMCU(uint8_t);
 String processor(const String& var);
+String desconvercionhora(uint8_t,uint8_t);
+uint8_t convercionhora(uint8_t, String);
 //█████████████████████████████████████████████████████████████████████████████████
 
 struct save{uint8_t hora;uint8_t temp;uint8_t lvl;};
-String ssid = "ProyectosTecnologicos";
-String password = "proy.Tec";
+String ssid = "Jere";
+String password = "chucotest";
 //█████████████████████████████████████████████████████████████████████████████████
 
+struct save_data{ uint8_t hour; uint8_t level; uint8_t temp;};
+save_data save[3]; 
 String TVal = "60";
 String LVal = "70";
 String HVal = "12:30";
@@ -21,13 +25,22 @@ uint8_t TEMP_VAL=0;
 uint8_t LVL_VAL=0;
 uint8_t HOUR_VAL=0;
 uint8_t MINUTE_VAL=0;
+
+uint8_t Struct=0;
+uint8_t Temp_Max=0;
+uint8_t Temp_Min=0;
+uint8_t Level_Max=0;
+uint8_t Level_Min=0;
+
+
 bool CHARGING_STATE=0;
 bool HEATING_STATE=0;
 uint8_t AUTOTEMP_STATE=0;
 uint8_t AUTOLVL_STATE=0;
 
-uint8_t InitComunication=false;
-uint8_t ComunicationError=false;
+uint8_t ActualIndividualDataPos=0;
+bool ConvertString=true;
+bool ComunicationError=false;
 
 //█████████████████████████████████████████████████████████████████████████████████
 // Asigna el webserver al puerto 80 de la red wifi
@@ -43,13 +56,10 @@ void setup(){
  
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.println("Connecting to WiFi..");
-    InitComunication=true;
-  }
+  while (WiFi.status() == WL_CONNECTED) {}
 
   // Print ESP32 Local IP Address
-  Serial.println(WiFi.localIP());
+
 
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){request->send(LittleFS, "/index.html", String(), false, processor);});
@@ -62,7 +72,6 @@ void setup(){
     {
       String inputMessage = request->getParam("hour")->value();
       HVal = inputMessage;
-      Serial.println(HVal);
       request->send(LittleFS, "/index.html", String(), false, processor);
     }
   });
@@ -90,53 +99,67 @@ void setup(){
   //detectaa cuando se redirige (producto de que se presiona un boton) en alguna pagina y realiza algo
   server.on("/STATEMP", HTTP_GET, [](AsyncWebServerRequest *request){
     HEATING_STATE= !HEATING_STATE; 
-    Serial_Send_UNO(65);
+    Serial_Send_NODEMCU(1);
     request->send(LittleFS, "/index.html", String(), false, processor);
   }); 
 
   server.on("/STALVL", HTTP_GET, [](AsyncWebServerRequest *request){
     CHARGING_STATE= !CHARGING_STATE;
-    Serial_Send_UNO(65); 
+    Serial_Send_NODEMCU(65); 
     request->send(LittleFS, "/index.html", String(), false, processor);
   });
 
   server.on("/SETEMP", HTTP_GET, [](AsyncWebServerRequest *request){
     AUTOTEMP_STATE++; 
-    Serial_Send_UNO(65);
+    if (AUTOTEMP_STATE==1)Temp_Min=TEMP_VAL;
+    if (AUTOTEMP_STATE==2)Temp_Max=TEMP_VAL;
+    if (AUTOTEMP_STATE==3)Serial_Send_NODEMCU(4);
     if(AUTOTEMP_STATE>3)AUTOTEMP_STATE=0; 
     request->send(LittleFS, "/index.html", String(), false, processor);
   });
 
   server.on("/SETLVL", HTTP_GET, [](AsyncWebServerRequest *request){
     AUTOLVL_STATE++; 
-    Serial_Send_UNO(65);
+    if (AUTOLVL_STATE==1)Level_Min=TEMP_VAL;
+    if (AUTOLVL_STATE==2)Level_Max=TEMP_VAL;
+    if (AUTOLVL_STATE==3)Serial_Send_NODEMCU(5);
     if(AUTOLVL_STATE>3)AUTOLVL_STATE=0; 
     request->send(LittleFS, "/index.html", String(), false, processor);
   });
 
   server.on("/TIMERSET", HTTP_GET, [](AsyncWebServerRequest *request){ 
-    Serial_Send_UNO(65);
     request->send(LittleFS, "/config.html", String(), false, processor);
   });
 
 
   server.on("/S1", HTTP_GET, [](AsyncWebServerRequest *request){
-    Serial_Send_UNO(65);
+    save[0].hour=convercionhora(1, HVal);
+    save[0].level=convercionhora(2, LVal);
+    save[0].temp=convercionhora(2, TVal);
+    Struct=0;
+    Serial_Send_NODEMCU(3);
     request->send(LittleFS, "/config.html", String(), false, processor);
   });
 
   server.on("/S2", HTTP_GET, [](AsyncWebServerRequest *request){
-    Serial_Send_UNO(65);
+    save[1].hour=convercionhora(1, HVal);
+    save[1].level=convercionhora(2, LVal);
+    save[1].temp=convercionhora(2, TVal);
+    Struct=1;
+    Serial_Send_NODEMCU(3);
     request->send(LittleFS, "/config.html", String(), false, processor);
   });
 
   server.on("/S3", HTTP_GET, [](AsyncWebServerRequest *request){
-    Serial_Send_UNO(65);
+    save[2].hour=convercionhora(1, HVal);
+    save[2].level=convercionhora(2, LVal);
+    save[2].temp=convercionhora(2, TVal);
+    Struct=2;
+    Serial_Send_NODEMCU(3);
     request->send(LittleFS, "/config.html", String(), false, processor);
   });
 
   server.on("/RETURN", HTTP_GET, [](AsyncWebServerRequest *request){ 
-    Serial_Send_UNO(65);
     request->send(LittleFS, "/index.html", String(), false, processor);
   });
   server.begin();
@@ -149,71 +172,36 @@ void loop()
 
 }
 
-void Serial_Send_UNO(uint8_t WhatSend)
+void Serial_Send_NODEMCU(uint8_t WhatSend)
   {
-    uint8_t MessagePoss;
-    if (InitComunication==true)MessagePoss=0;
+    String message;
     switch (WhatSend){
       case 1:
-        if (ComunicationError==false && InitComunication==true && MessagePoss<=5)//&&Send_time>=1000) BUSCAR INTERRUPCIONES EN ESP
-        {
-            switch (MessagePoss)
-            {
-              case 0:
-                Serial.println("S_""SSID"":""Pass");
-                 MessagePoss++;
-                 //Send_time=0;
-                break;
-              case 1:
-                Serial.println("K_HORA:TEMP:LVL:0");
-                MessagePoss++;
-                //Send_time=0;
-                break;
-              case 2:
-                Serial.println("K_HORA:TEMP:LVL:1");
-                MessagePoss++;
-                //Send_time=0;
-                break;
-              case 3:
-                Serial.println("K_HORA:TEMP:LVL:2");
-                MessagePoss++;
-                //Send_time=0;
-                break;
-              case 4:
-                Serial.println("J_255:TEMPMIN:TEMPMAX:3");
-                MessagePoss++;
-                //Send_time=0;
-                break;
-              case 5:
-                Serial.println("V_255:TEMPMIN:TEMPMAX:3");
-                InitComunication=false;
-                MessagePoss=0;
-                //Send_time=0;
-                break;
-            // delay de 1 seg
-            } //Send_time =0;
-        }
+        if (HEATING_STATE == true)message = "ON";
+        if (HEATING_STATE == false)message = "OFF";
+        Serial.println("H_"+TVal+":"+message);
         break;
       case 2:
-        if (ComunicationError==false && InitComunication==false)
-          {
-              Serial.println("U_TEMP:LVL:HORA:STATE");
-          }
+        if (CHARGING_STATE == true)message = "ON";
+        if (CHARGING_STATE == false)message = "OFF";
+        Serial.println("C_"+LVal+":"+message);
+        break;
       case 3:
-        if (ComunicationError==false && InitComunication==false)
-          {
-            Serial.println("K_HORA:TEMP:LVL:STRUCTPOS");
-          }
+    
+        Serial.println("K_"+String(save[Struct].hour)+":"+desconvercionhora(2,save[Struct].temp)+":"+desconvercionhora(3,save[Struct].level)+":"+String(Struct));
+        break;
       case 4:
-        if (ComunicationError==false && InitComunication==false)
-          {
-            Serial.println("J_255:TEMPMIN:TEMPMAX:3");
-          }
+        Serial.println("J_255:"+String(Temp_Min)+":"+String(Temp_Max)+":3");
+        break;
       case 5:
-        if (ComunicationError==false && InitComunication==false)
-          {
-            Serial.println("V_255:LVLMIN:LVLMAX:4");
-          }
+        Serial.println("V_255:"+String(Level_Min)+":"+String(Level_Max)+":3");
+        break;
+      case 6:
+        Serial.println("E_ERROR");// Si no entiende un mensaje envia error
+        break;
+      case 7:
+        Serial.println("O_OK");// Si entiende el mensaje manda ok
+        break;
     }
     
   }
@@ -256,3 +244,149 @@ if(var == "STLA"){
 return String();
 }
 
+void Serial_Read_UNO(){
+  String Serial_Input;
+  uint8_t StringLength;
+  String Individualdata[4];
+  char input;
+
+  Serial_Input=Serial.readString();// iguala el string del serial a un string imput
+  StringLength= Serial_Input.length();// saca el largo del string
+  Serial.println(Serial_Input);// Solo de verificacion (eliminar en el final)
+  input=Serial_Input.charAt(0); // toma el char de comando (el primer char usualmente una letra)
+
+  for (uint8_t CharPos = 2; CharPos <= StringLength; CharPos++) // comeinza desde la posicion 2 del char (tras el _) y toma todos los datos
+  {
+      if(Serial_Input.charAt(CharPos)==':')ActualIndividualDataPos++; //si hay : divide los datos
+      if(Serial_Input.charAt(CharPos)!=':' && Serial_Input.charAt(CharPos)!='_' && Serial_Input.charAt(CharPos)!='/')// si no es nungun caracter especial:
+        {
+          if(Serial_Input.charAt(CharPos-1)==':' || Serial_Input.charAt(CharPos-1)=='_')Individualdata[ActualIndividualDataPos]=Serial_Input.charAt(CharPos);//si es el primer digito lo iguala
+          else Individualdata[ActualIndividualDataPos]+=Serial_Input.charAt(CharPos);//si es el segundo en adelante lo suma
+        }
+      if(CharPos==StringLength)ConvertString=true;// activa el comando final (flag)
+  } 
+  
+  switch (input)//dependiendo del char de comando
+  {
+  case 'K':
+    if (ConvertString==true)
+      {
+        Struct=Individualdata[3].toInt();
+        if (Struct<=2 && Struct>=0)
+          {
+            save[Struct].hour=Individualdata[0].toInt();
+            save[Struct].level=Individualdata[1].toInt();
+            save[Struct].temp=Individualdata[2].toInt();
+            ActualIndividualDataPos=0;
+            ConvertString=false;
+            ComunicationError=false;
+          }
+        else Serial.println("error");
+      }
+    break;
+
+  case 'J':
+    if (ConvertString==true)
+      {
+        Temp_Min=Individualdata[1].toInt();// tempin
+        Temp_Max=Individualdata[1].toInt();// tempmax
+        ActualIndividualDataPos=0;
+        ConvertString=false;
+        ComunicationError=false;
+      }
+    break;
+      case 'V':
+    if (ConvertString==true)
+      {
+        Level_Min=Individualdata[0].toInt();// lvlmin
+        Level_Max=Individualdata[1].toInt();// lvlmax
+        ActualIndividualDataPos=0;
+        ConvertString=false;
+        ComunicationError=false;
+      }
+    break;
+    case '?':
+    if (ConvertString==true)
+      {
+        if(Individualdata[0]=="ERROR")
+        {
+              Serial.println("?_RESET");
+              ComunicationError=true;
+        }
+        if(Individualdata[0]=="NOTHING TO READ")
+        {
+              Serial.println("?_RESET");
+              ComunicationError=true;
+        }
+        ActualIndividualDataPos=0;
+        ConvertString=false;
+      }
+    break;
+  default:
+    Serial.println("?_NOTHING TO READ");
+    break;
+  }
+}
+
+String desconvercionhora(uint8_t function,uint8_t save)
+{
+  uint8_t var1_deconvert=0;//solo una variable (_deconvert  nos evita modificar variables globales como bldos)
+  uint8_t var2_deconvert=0;
+  uint8_t resto_deconvert=0;
+  String returned;
+  // todo el dia con la mielcita jere ¯\_(ツ)_/¯ ??
+  switch (function)
+    {
+      case 1:
+        resto_deconvert= (save) % 4;
+        var1_deconvert= (save-resto_deconvert)/4;
+        var2_deconvert=resto_deconvert*15;
+        returned= String(var1_deconvert)+':'+String(var2_deconvert);
+        return returned;
+      break;
+      case 2:
+        returned= String(save);
+        return returned;
+      break;
+      case 3:
+        returned= String(save);
+        return returned;
+      break;
+      default:
+        return "ERROR";
+        break;
+    }
+}
+
+uint8_t convercionhora(uint8_t function, String toconvert)
+{
+  uint8_t var1_convert; // solo una variable (_convert  nos evita modificar variables globales como bldos)
+  uint8_t var2_convert; // solo una variable
+  uint8_t resto_convert; //guarda el resto del calculo de tiempo
+  switch (function)
+  {
+    case 1:
+      var1_convert=(toconvert.charAt(0)- '0')*10;// toma el valor del primer digito del string y lo convierte en int (numero de base 10)
+      var1_convert+=(toconvert.charAt(1)-'0');// toma el valor del segundo digito
+      var1_convert=var1_convert*4;//multiplica la hora x 4 (la proporcionalidad esta en la documentacion)
+
+      var2_convert=(toconvert.charAt(3)- '0')*10;//lo mismo que en el var 1 pero con minutos (10:[puntero aca]0)
+      var2_convert+=(toconvert.charAt(4)-'0');//lo mismo que en el var 1 pero con minutos
+      resto_convert=var2_convert%15; //saca el resto (ejemplo 7/5 resto 2)
+      if(resto_convert<8) var2_convert= var2_convert-resto_convert; //utiliza el resto para redondear abajo (Esto se da pq en el propio diseño del sistema decidimos guardar todas las horas en un char)
+      else var2_convert=var2_convert+15-resto_convert;// utiliza el resto para redondear arriba
+      var2_convert=var2_convert/15;// convierte los minutos en la proporcion del char (1 entero = 15 minutos)
+
+      var1_convert+=var2_convert;// suma horas y minutos
+      return var1_convert;
+      break;
+    case 2:
+      var1_convert= toconvert.toInt();// hace magia y despues de tirar magia convierte el string en un int (fua ta dificil la conversion de ete)
+      return var1_convert;
+      break;
+    default:
+      var2_convert= toconvert.toInt();// mismo sistema
+      return var2_convert;
+      break;
+  }
+}
