637547e27dc8cafda26d07e90f792da823cfd7e2
Teo pc colegio <teo.maximiliano.castillo2@gmail.com>
2022-08-29 16:49:45 -0300

    cum

--- Proyecto Final/Codigos hechos/Arduino/Codigo control de temperatura automatico/src/main.cpp
+++ Proyecto Final/Codigos hechos/Arduino/Codigo control de temperatura automatico/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
-#include <LiquidCrystal_I2C.h>
+//include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
@@ -11,8 +12,9 @@ int temperatura_final = 40;
 int min_temp_ini = 40;
 int maxima_temp_fin = 80;
 bool confirmar = false;
+int temperatura_actual = 0;
 
-LiquidCrystal_I2C lcd(0x27,16,2);
+//LiquidCrystal_I2C lcd(0x27,16,2);
 
 const int onewire = 6;
 OneWire sensor_t(onewire);
@@ -20,10 +22,15 @@ DallasTemperature Sensor_temp(&sensor_t);
 
 void setup() {
   pinMode(resistencia, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  control_de_temp_auto(temperatura_inicial,temperatura_final);
+  //control_de_temp_auto(temperatura_inicial,temperatura_final);
+  Sensor_temp.requestTemperatures();
+  temperatura_actual = Sensor_temp.getTempCByIndex(0);
+  Serial.print("temperatura:");
+  Serial.println(temperatura_actual);
 }
 
 
